18ebb11195161190d7f906b97d65877e140ee010
erofimova <e.erofimova@gmail.com>
2024-10-01 21:33:36 +0300

    Aquarium

--- 6. Aquarium/Program.cs
+++ 6. Aquarium/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());  
+double percentage = double.Parse(Console.ReadLine()); 
+
+int volumeInCm3 = length * width * height;
+double volumeInLiters = volumeInCm3 / 1000.0;
+double usableVolume = volumeInLiters * (1 - percentage / 100);
+Console.WriteLine($"{usableVolume:F2}");
