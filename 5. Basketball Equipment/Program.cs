e932c9304e2532aba790c41642d927f0f95e8c1f
erofimova <e.erofimova@gmail.com>
2024-10-01 19:27:34 +0300

    Basketball project

--- 5. Basketball Equipment/Program.cs
+++ 5. Basketball Equipment/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int yearFee = int.Parse(Console.ReadLine());
+double sneekers = yearFee * 0.6;
+double uniforms = sneekers * 0.8;
+double balls = uniforms / 4;
+double accessories = balls / 5;
+
+double totalSum = sneekers + uniforms + balls + accessories + yearFee;
+Console.WriteLine(totalSum);
