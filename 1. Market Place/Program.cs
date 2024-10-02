dcd9b62cb4e1e4a430887f6a13a6ac66a2110621
erofimova <e.erofimova@gmail.com>
2024-10-02 22:28:05 +0300

    Market Place

---
+++ 1. Market Place/Program.cs
@@ -0,0 +1,38 @@
+﻿string product = Console.ReadLine();
+string day = Console.ReadLine();
+
+if (product == "Banana")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("2.50");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine("2.70");
+    }
+}
+else if (product == "Apple")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("1.30");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine("1.60");
+    }
+
+}
+else if (product == "Kiwi")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("2.20");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine("3.00");
+    }
+
+}
