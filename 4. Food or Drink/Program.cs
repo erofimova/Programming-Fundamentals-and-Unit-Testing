1ddfe5cb8198b77006a0dd1f8a987b7ecba24f6e
erofimova <e.erofimova@gmail.com>
2024-10-02 23:50:19 +0300

    Food or Drink

---
+++ 4. Food or Drink/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System;
+string product = Console.Readline();
+
+if (product == curry || noodles || sushi || spaghetti || bread)
+{
+    Console.WriteLine("food");
+}
+else if (product == tea || water || coffee || juice)
+{
+    Console.WriteLine("drink");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
