21e037503e7e3ba8716a6d3cf9b9b8d7fe7798d3
erofimova <e.erofimova@gmail.com>
2024-10-02 23:08:09 +0300

    Largest Number Out Of Three

---
+++ 2. Largest Number Out Of Three/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+int thirdNum = int.Parse(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+    if (firstNum > thirdNum)
+    {
+        Console.WriteLine($"{firstNum}");
+    }
+    else { Console.WriteLine($"{thirdNum}"); }
+}
+else if (secondNum > thirdNum)
+{
+    Console.WriteLine($"{secondNum}");
+}
+else { Console.WriteLine($"{thirdNum}"); }
