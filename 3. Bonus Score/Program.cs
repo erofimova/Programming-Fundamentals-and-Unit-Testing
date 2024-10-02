0e41ebf46269495656258ec97883b50a7bd4abdb
erofimova <e.erofimova@gmail.com>
2024-10-02 23:22:58 +0300

    Bonus Score

---
+++ 3. Bonus Score/Program.cs
@@ -0,0 +1,18 @@
+﻿int points = int.Parse(Console.ReadLine());
+int result;
+
+if (points >= 0 && points <= 3)
+{
+    result = points + 5;
+    Console.WriteLine(result);
+}
+else if (points >= 4 && points <= 6)
+{
+    result = points + 15;
+    Console.WriteLine(result);
+}
+else if (points >= 7 && points <= 9)
+{
+    result = points + 20;
+    Console.WriteLine(result);
+}
