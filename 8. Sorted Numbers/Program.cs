b8c603adc2828ee0ce412d4da55118427e8f44e7
erofimova <e.erofimova@gmail.com>
2024-10-03 23:20:54 +0300

    Sorted Numbers

---
+++ 8. Sorted Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+bool ascending = a < b && b < c;
+bool descending = a > b && b > c;
+
+if (ascending)
+{
+    Console.WriteLine("Ascending");
+}
+else if (descending)
+{
+    Console.WriteLine("Descending");
+}
+else  Console.WriteLine("Not sorted"); 
