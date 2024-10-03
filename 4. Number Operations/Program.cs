a64d6a12c650fb2bb0dba0f8246d06b1f4b5281e
erofimova <e.erofimova@gmail.com>
2024-10-04 00:09:58 +0300

    Number Operations

---
+++ 4. Number Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+string mathOperator = Console.ReadLine();
+
+double result = 0;
+
+switch (mathOperator)
+{
+    case "+": result = num1 + num2; break;
+    case "-": result = num1 - num2; break;
+    case "*": result = num1 * num2; break;
+    case "/": result = num1 / num2; break;
+}
+
+Console.WriteLine($"{num1} {mathOperator} {num2} = {result:F2}");
