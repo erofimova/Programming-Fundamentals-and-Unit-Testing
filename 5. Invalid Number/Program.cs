aa609fb59ebd9c96b22b2a3088b0519e2fe8a287
erofimova <e.erofimova@gmail.com>
2024-10-03 23:53:16 +0300

    ATM

--- 5. Invalid Number/Program.cs
+++ 5. Invalid Number/Program.cs
@@ -1,15 +1,8 @@
-﻿//using static System.Runtime.InteropServices.JavaScript.JSType;
-//int num = int.Parse(Console.ReadLine());
-
-//bool isValid = (num >= 100 && num <= 200) || num == 0;
-//if (!isValid)
-//{
-//    Console.WriteLine("invalid");
-//}
-
+﻿
 int num = int.Parse(Console.ReadLine());
 
-if (num != 0 && (num > 200 && num < 100))
+bool isValid = (num >= 100 && num <= 200) || num == 0;
+if (!isValid)
 {
     Console.WriteLine("invalid");
 }
