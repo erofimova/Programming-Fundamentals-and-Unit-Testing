628055c538a16a9ee18979e994b7638267da794e
erofimova <e.erofimova@gmail.com>
2024-10-03 23:27:56 +0300

    Cinema

---
+++ 9. Cinema/Program.cs
@@ -0,0 +1,22 @@
+﻿string movieType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+
+double price = 0;
+
+switch (movieType)
+{
+    case "Premiere":
+        price = 12.00;
+        break;
+    case "Normal":
+        price = 7.50;
+        break;
+    case "Discount":
+        price = 5.00;
+        break;
+}
+
+double total = rows * seats * price;
+
+Console.WriteLine($"{total:F2}");
