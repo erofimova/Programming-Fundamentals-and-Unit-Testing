c97c1341b87c221c07a326fed3d91c5a7a9f61e2
erofimova <e.erofimova@gmail.com>
2024-10-05 00:48:38 +0300

    Vacation Expenses

---
+++ 5. Vacation Expenses/Program.cs
@@ -0,0 +1,74 @@
+﻿string season = Console.ReadLine();
+string accommodationType = Console.ReadLine();
+int days = int.Parse(Console.ReadLine());
+double pricePerNight = 0;
+double totalPrice = 0;
+double priceWithDiscount = 0;
+
+switch (season)
+{
+    case "Spring":
+        if (accommodationType == "Hotel")
+        {
+            pricePerNight = 30;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.8;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        else if (accommodationType == "Camping")
+        {
+            pricePerNight = 10;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.8;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        break;
+    case "Summer":
+        if (accommodationType == "Hotel")
+        {
+            pricePerNight = 50;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 1;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        else if (accommodationType == "Camping")
+        {
+            pricePerNight = 30;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 1;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        break;
+    case "Autumn":
+        if (accommodationType == "Hotel")
+        {
+            pricePerNight = 20;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.7;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        else if (accommodationType == "Camping")
+        {
+            pricePerNight = 15;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.7;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        break;
+    case "Winter":
+        if (accommodationType == "Hotel")
+        {
+            pricePerNight = 40;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.9;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        else if (accommodationType == "Camping")
+        {
+            pricePerNight = 10;
+            totalPrice = pricePerNight * days;
+            priceWithDiscount = totalPrice * 0.9;
+            Console.WriteLine($"{priceWithDiscount:F2}");
+        }
+        break;
+}
