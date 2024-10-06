42f509a0d1aebc1e1deb3d19f40c4519c22f38b7
erofimova <e.erofimova@gmail.com>
2024-10-06 23:08:39 +0300

    Working Hours

---
+++ 7. Working Hours/Program.cs
@@ -0,0 +1,8 @@
+﻿int hours = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+if (hours >= 10 && hours <= 18 && (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday"))
+{
+    Console.WriteLine("open");
+}
+else Console.WriteLine("closed");
