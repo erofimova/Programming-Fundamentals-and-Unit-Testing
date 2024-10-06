87bca8d91f947821a1588afd48fd2f8514fe401d
erofimova <e.erofimova@gmail.com>
2024-10-06 23:30:18 +0300

    Sum Seconds

---
+++ 9. Sum Seconds/Program.cs
@@ -0,0 +1,9 @@
+﻿int athletTime1 = int.Parse(Console.ReadLine());
+int athletTime2 = int.Parse(Console.ReadLine());
+int athletTime3 = int.Parse(Console.ReadLine());
+
+int secondsTime = athletTime1 + athletTime2 + athletTime3;
+int minutes = secondsTime / 60;
+int seconds = secondsTime % 60;
+
+Console.WriteLine($"{minutes}:{seconds:D2}");
