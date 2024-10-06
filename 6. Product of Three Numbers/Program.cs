3e1c268f59d0395b201b6e012e16799795d035d2
erofimova <e.erofimova@gmail.com>
2024-10-06 22:46:30 +0300

    Product of Three Numbers

---
+++ 6. Product of Three Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 == 0 || num2 == 0 || num3 == 0)
+{
+    Console.WriteLine("zero");
+}
+else if (num1 < 0 && num2 < 0 && num3 < 0) { Console.WriteLine("negative"); }
+else if (num1 > 0 && num2 > 0 && num3 < 0) { Console.WriteLine("negative"); }
+else if (num1 > 0 && num2 < 0 && num3 > 0) { Console.WriteLine("negative"); }
+else if (num1 < 0 && num2 > 0 && num3 > 0) { Console.WriteLine("negative"); }
+
+else { Console.WriteLine("positive"); }
