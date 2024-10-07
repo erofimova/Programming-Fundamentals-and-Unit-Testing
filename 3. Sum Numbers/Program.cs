7670cbe48f3221c6be07da62288cde5c52c04a78
erofimova <e.erofimova@gmail.com>
2024-10-07 10:29:02 +0300

    Sum Numbers

---
+++ 3. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int count = int.Parse(Console.ReadLine());
+double sum = 0;
+
+for (int num = 1; num <= count; num++)
+{
+    double value = double.Parse(Console.ReadLine());
+    sum = sum + value;
+}
+Console.WriteLine(sum);
