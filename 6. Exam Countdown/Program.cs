c2001103ccac86a4686bb611272421f2c693a843
erofimova <e.erofimova@gmail.com>
2024-10-07 10:51:03 +0300

    Exam Countdown

---
+++ 6. Exam Countdown/Program.cs
@@ -0,0 +1,7 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+for (int day = count; day >= 1; day--)
+{
+    Console.WriteLine($"{day} days before the exam");
+}
+Console.WriteLine("The exam has come");
