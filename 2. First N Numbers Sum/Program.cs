4b2f4e0a89fcfb4d58a475e11cc9d5d0f742230c
erofimova <e.erofimova@gmail.com>
2024-10-07 10:18:44 +0300

    First N Numbers Sum

---
+++ 2. First N Numbers Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿int endNumber = int.Parse(Console.ReadLine());
+int startNumber = 1;
+int sum = 0;
+for (int num = startNumber; num <= endNumber; num++)
+{
+    if (num != endNumber)
+    {
+        Console.Write(num + "+");
+       
+    }
+    else { Console.Write(num + "="); }
+    sum = sum + num;
+}
+Console.WriteLine(sum);
