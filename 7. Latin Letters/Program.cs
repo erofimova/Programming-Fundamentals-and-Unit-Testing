ae23dfbcceb5d6eda3c3cb9fc7afa92683421a1f
erofimova <e.erofimova@gmail.com>
2024-10-07 11:03:02 +0300

    Latin Letters

---
+++ 7. Latin Letters/Program.cs
@@ -0,0 +1,7 @@
+﻿char firstLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine());
+
+for (int letter = firstLetter; letter <= endLetter; letter++)
+{
+    Console.Write((char)letter + " ");
+}
