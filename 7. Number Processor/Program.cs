a8f341b56476bdfcb5b18c29a90d111615eab688
erofimova <e.erofimova@gmail.com>
2024-10-08 23:39:56 +0300

    Number Processor

---
+++ 7. Number Processor/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = int.Parse(Console.ReadLine());
+string commands = Console.ReadLine();
+
+while (commands != "End")
+{
+    if (commands == "Inc")
+    {
+        number += 1;
+        commands = Console.ReadLine();
+    }
+    else if (commands == "Dec")
+    {
+        number -= 1;
+        commands = Console.ReadLine();
+    }
+
+}
+Console.WriteLine(number);
+
+
