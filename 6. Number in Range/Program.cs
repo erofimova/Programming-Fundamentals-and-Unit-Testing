265750aa14758dfb1d51562d8d1b91db4bae6b88
erofimova <e.erofimova@gmail.com>
2024-10-08 23:20:57 +0300

    Number in Range

---
+++ 6. Number in Range/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (number >= 0 && number <= 100)
+    {
+        Console.WriteLine(number);
+        break;
+    }
+    else
+    {
+        number = int.Parse(Console.ReadLine());
+
+    }
+}
