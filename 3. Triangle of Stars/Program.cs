75ab96f700499e83727c29e02e10dff685a3e6bb
erofimova <e.erofimova@gmail.com>
2024-10-10 09:46:31 +0300

    Triangle of Stars

---
+++ 3. Triangle of Stars/Program.cs
@@ -0,0 +1,10 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= size; row++)
+{
+    for (int star = 1; star <= row; star++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine();
+}
