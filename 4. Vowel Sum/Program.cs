b4a7596d9539863d2f6e5c5bc685f017dbb8af22
erofimova <e.erofimova@gmail.com>
2024-10-10 23:16:08 +0300

    Vowel Sum

---
+++ 4. Vowel Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    char c = char.Parse(Console.ReadLine());
+    switch (c)
+    {
+        case 'a':
+            sum += 1;
+            break;
+        case 'e':
+            sum += 2;
+            break;
+        case 'i':
+            sum += 3;
+            break;
+        case 'o':
+            sum += 4;
+            break;
+        case 'u':
+            sum += 5;
+            break;
+    }
+}
+Console.WriteLine(sum);
+
