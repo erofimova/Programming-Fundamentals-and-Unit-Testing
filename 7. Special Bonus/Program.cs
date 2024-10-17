1a738b0e200dfd57379a25239662fc91dabeda37
erofimova <e.erofimova@gmail.com>
2024-10-17 13:53:43 +0300

    Special Bonus

---
+++ 7. Special Bonus/Program.cs
@@ -0,0 +1,14 @@
+﻿int stopNum = int.Parse(Console.ReadLine());
+
+int currentNum = int.Parse(Console.ReadLine());
+
+int previousNum = 0;
+
+while (currentNum != stopNum)
+{
+    previousNum = currentNum;
+
+    currentNum = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(previousNum * 1.2);
