ad11389f8a8ff8ad2404fdfc812ad44735ec69f8
erofimova <e.erofimova@gmail.com>
2024-10-17 13:50:37 +0300

    Special Number

---
+++ 6. Special Number/Program.cs
@@ -0,0 +1,26 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int token = num;
+bool isSpecial = true;
+
+while (token > 0)
+{
+    int lastDigit = token % 10;
+
+    token /= 10;
+
+    if (num % lastDigit != 0)
+    {
+        isSpecial = false;
+        break;
+    }
+}
+
+if (isSpecial)
+{
+    Console.WriteLine($"{num} is special");
+}
+else
+{
+    Console.WriteLine($"{num} is not special");
+}
