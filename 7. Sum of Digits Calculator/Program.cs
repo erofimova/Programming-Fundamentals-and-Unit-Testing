b1a726e530f56e7689185b2bf39c1e59dc322760
erofimova <e.erofimova@gmail.com>
2024-10-18 19:18:55 +0300

    Sum of Digits Calculator

---
+++ 7. Sum of Digits Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+
+while (input != "End")
+{
+    int token = int.Parse(input);
+    int sum = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        int digit = token % 10;
+        sum += digit;
+        token /= 10;
+
+    }
+    Console.WriteLine($"Sum of digits = {sum}");
+    input = Console.ReadLine();
+}
+Console.WriteLine("Goodbye");
