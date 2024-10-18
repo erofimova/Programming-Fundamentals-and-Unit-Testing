92844853445dda861508a66baa89154cbf4954ba
erofimova <e.erofimova@gmail.com>
2024-10-18 20:16:34 +0300

    Sign of Integer Numbers

---
+++ 1. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = int.Parse(Console.ReadLine());
+PrintNumType(num);
+
+static void PrintNumType(int num)
+{
+    if (num > 0)
+    {
+        Console.WriteLine($"The number {num} is positive.");
+    }
+    else if (num < 0)
+    {
+        Console.WriteLine($"The number {num} is negative.");
+    }
+    else
+    {
+
+        Console.WriteLine($"The number {num} is zero.");
+    }
+}
