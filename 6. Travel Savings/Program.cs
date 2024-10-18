a2bdebfd4b941108c86497284ec2c62e72ec56c8
erofimova <e.erofimova@gmail.com>
2024-10-18 19:05:10 +0300

    Travel Savings

---
+++ 6. Travel Savings/Program.cs
@@ -0,0 +1,18 @@
+﻿string destination = Console.ReadLine();
+
+
+while (destination != "End")
+{
+    double sum = 0;
+    double budget = double.Parse(Console.ReadLine());
+
+    while (sum < budget)
+    {
+        sum += double.Parse(Console.ReadLine());
+        Console.WriteLine($"Collected: {sum:F2}");
+    }
+    Console.WriteLine($"Going to {destination}!");
+
+    destination = Console.ReadLine();
+
+}
