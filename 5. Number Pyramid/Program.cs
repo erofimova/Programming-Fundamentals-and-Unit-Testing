7349d34b1e723c1a999b1dc4a92c8fb8b9887a01
erofimova <e.erofimova@gmail.com>
2024-10-18 18:43:21 +0300

    Number Pyramid

---
+++ 5. Number Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+int currentNum = 1;
+
+for (int row = 1; row <= n; row++)
+{
+    for (int i = 1; i <= row; i++)
+    {
+        Console.Write(currentNum + " ");
+        currentNum++;
+        if (currentNum > n)
+        {
+            break;
+        }
+
+    }
+    if (currentNum > n)
+    {
+        break;
+    }
+    Console.WriteLine();
+}
