e32c8cfc4b378010c344d0828efd3253ba9c971f
erofimova <e.erofimova@gmail.com>
2024-10-21 11:33:07 +0300

    Multiplication Sign

---
+++ 3. Multiplication Sign/Program.cs
@@ -0,0 +1,23 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+ResultSign(num1, num2, num3);
+
+static void ResultSign(int num1, int num2, int num3)
+{
+    string result = "";
+    if (num1 == 0 || num2 == 0 || num3 == 0)
+    {
+        result = "zero";
+    }
+    else if ((num1 > 0 && num2 > 0 && num3 > 0) || (num1 < 0 && num2 < 0 && num3 > 0) || (num1 > 0 && num2 < 0 && num3 < 0) || (num1 < 0 && num2 > 0 && num3 < 0))
+    {
+        result = "positive";
+    }
+    else
+    {
+        result = "negative";
+    }
+    Console.WriteLine(result);
+}
