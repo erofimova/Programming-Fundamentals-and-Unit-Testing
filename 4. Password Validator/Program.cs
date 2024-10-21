ec7a2e3a76b154127caa9e846249952afc18299e
erofimova <e.erofimova@gmail.com>
2024-10-21 11:41:49 +0300

    Password Validator

---
+++ 4. Password Validator/Program.cs
@@ -0,0 +1,67 @@
+﻿string password = Console.ReadLine();
+
+if (ChechPassContainsBetweenSixAndTenCharacters(password)
+    && CheckPassConsistOnlyOfLettersAndDigits(password)
+    && CheckPassHaveAtLeastTwoDigits(password))
+{
+    Console.WriteLine("Password is valid");
+}
+
+if (!ChechPassContainsBetweenSixAndTenCharacters(password))
+{
+    Console.WriteLine("Password must be between 6 and 10 characters");
+}
+
+if (!CheckPassConsistOnlyOfLettersAndDigits(password))
+{
+    Console.WriteLine("Password must consist only of letters and digits");
+}
+
+if (!CheckPassHaveAtLeastTwoDigits(password))
+{
+    Console.WriteLine("Password must have at least 2 digits");
+}
+
+static bool ChechPassContainsBetweenSixAndTenCharacters(string pass)
+{
+    if (pass.Length >= 6 && pass.Length <= 10)
+        return true;
+    else
+        return false;
+}
+
+static bool CheckPassConsistOnlyOfLettersAndDigits(string password)
+{
+    for (int i = 0; i < password.Length; i++)
+    {
+        char currentLetter = password[i];
+
+        if (!char.IsLetterOrDigit(currentLetter))
+            return false;
+    }
+
+    return true;
+}
+
+static bool CheckPassHaveAtLeastTwoDigits(string pass)
+{
+    int countDigits = 0;
+
+    for (int i = 0; i < pass.Length; i++)
+    {
+        char currentLetter = pass[i];
+
+        if (char.IsDigit(currentLetter))
+        {
+            countDigits++;
+
+            if (countDigits == 2)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
