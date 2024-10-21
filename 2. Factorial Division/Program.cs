676d81952afd765545e059012c44fac9ce2284f3
erofimova <e.erofimova@gmail.com>
2024-10-21 11:16:04 +0300

    Factorial Division

---
+++ 2. Factorial Division/Program.cs
@@ -0,0 +1,18 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+int firstFactorial = FindFactolial(firstNum);
+int secondFactorial = FindFactolial(secondNum);
+int result = firstFactorial / secondFactorial;
+Console.WriteLine(result);
+
+static int FindFactolial(int num)
+{
+    int factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+
+        factorial = factorial * i;
+    }
+    return factorial;
+}
