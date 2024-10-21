e44e1cc71ee42dc0f329931acad9abd988e94fd4
erofimova <e.erofimova@gmail.com>
2024-10-21 09:43:43 +0300

    Calculate Rectangle Area-void method

---
+++ 4. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,9 @@
+﻿int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+PrintRectangleArea(width, length);
+
+static void PrintRectangleArea(int width, int length)
+{
+    int area = width * length;
+    Console.WriteLine(area);
+}
