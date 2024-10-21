20ef4c6cad91d8b7b1d870e729fff10098785116
erofimova <e.erofimova@gmail.com>
2024-10-21 10:13:08 +0300

    Math Power

---
+++ 6. Math Power/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+PrintPower(num, power);
+static void PrintPower(int num, int power)
+{
+    Console.WriteLine(Math.Pow(num, power));
+}
