08aa8e872e67d70d5d9c613be9326e9d720f51dd
erofimova <e.erofimova@gmail.com>
2024-10-22 09:56:30 +0300

    Multiply Evens by Odds

---
+++ 5. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+static int GetSumOfEvenDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        if (lastDigit % 2 == 0)
+            sum += lastDigit;
+        num /= 10;
+    }
+    return sum;
+}
+
+static int GetSumOfOddDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        if (lastDigit % 2 != 0)
+            sum += lastDigit;
+        num /= 10;
+    }
+    return sum;
+}
+
+static int GetMultipleOfEvenAndOdds(int num)
+{
+    return GetSumOfEvenDigits(num) * GetSumOfOddDigits(num);
+}
