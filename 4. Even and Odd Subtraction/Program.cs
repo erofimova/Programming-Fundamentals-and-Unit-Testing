4c5c9f1ca703188dbdee459b750ad74ec365f55f
erofimova <e.erofimova@gmail.com>
2024-10-23 11:34:54 +0300

    Even and Odd Subtraction

---
+++ 4. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int sumOdd = 0;
+int sumEven = 0;
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sumEven += number;
+    }
+    else
+    {
+        sumOdd += number;
+    }
+}
+
+int diff = sumEven - sumOdd;
+Console.WriteLine(diff);
