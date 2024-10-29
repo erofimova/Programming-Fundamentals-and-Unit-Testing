d0d054c1ed0dd2b79ad87e5a5be4c1cfdea5cf8e
erofimova <e.erofimova@gmail.com>
2024-10-29 09:20:20 +0200

    Top Integers

---
+++ 7. Top Integers/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+for (int i = 0; i < numArray.Length - 1; i++)
+{
+    bool isGreater = true;
+
+    int currentElement = numArray[i];
+
+    for (int j = i + 1; j < numArray.Length; j++)
+    {
+        int nextRightElement = numArray[j];
+
+        if (nextRightElement >= currentElement)
+        {
+            isGreater = false;
+            break;
+        }
+    }
+
+    if (isGreater)
+    {
+        Console.Write(currentElement + " ");
+    }
+}
+
+Console.Write(numArray[numArray.Length - 1]);
