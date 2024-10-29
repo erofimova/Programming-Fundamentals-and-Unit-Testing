3ff1bf620580225080f2b8c7501040850f7ab55e
erofimova <e.erofimova@gmail.com>
2024-10-29 09:57:50 +0200

    Change List

---
+++ 1. Change List/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+List<string> cmdArg = Console.ReadLine().Split().ToList();
+
+while (cmdArg[0] != "end")
+{
+    string command = cmdArg[0];
+    if (command == "Delete")
+    {
+        int elementToDelete = int.Parse(cmdArg[1]);
+
+        
+        while (numbers.Contains(elementToDelete))      
+        {
+            numbers.Remove(elementToDelete);
+        }
+    }
+    else if (command == "Insert")
+    {
+        int elementToInsert = int.Parse(cmdArg[1]);
+        int index = int.Parse(cmdArg[2]);
+
+        numbers.Insert(index, elementToInsert);
+    }
+
+    cmdArg = Console.ReadLine().Split().ToList();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
