813f5c446bb63c398997dc7d42e9f4348e5ec63c
erofimova <e.erofimova@gmail.com>
2024-10-31 15:41:30 +0200

    1.0 Change List

---
+++ 1.0 Change List/Program.cs
@@ -0,0 +1,32 @@
+﻿List<int> numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToList();
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+
+    string[] commandParts = input.Split(" ");
+    string commandName = commandParts[0];
+
+    if (commandName == "Delete")
+    {
+        int elementForDelete = int.Parse(commandParts[1]);
+        while (numbers.Contains(elementForDelete))
+        {
+            numbers.Remove(elementForDelete);
+        }
+    }
+    else if (commandName == "Insert")
+    {
+        int elementForInsert = int.Parse(commandParts[1]);
+        int positionForInsert = int.Parse(commandParts[2]);
+        numbers.Insert(positionForInsert, elementForInsert);
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
