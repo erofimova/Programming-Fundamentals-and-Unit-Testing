84896187436da33c520401151bf45af675a50538
erofimova <e.erofimova@gmail.com>
2024-10-31 15:44:49 +0200

    House Party

---
+++ 2. House Party/Program.cs
@@ -0,0 +1,40 @@
+﻿int countCommands = int.Parse(Console.ReadLine());
+
+List<string> guestsList = new List<string>();
+
+for (int count = 1; count <= countCommands; count++)
+{
+    string command = Console.ReadLine();
+    string[] commandParts = command.Split(" ");
+    string name = commandParts[0]; //име на гост
+
+    if (commandParts.Length == 3)
+    {
+
+        if (guestsList.Contains(name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+        }
+        else
+        {
+            guestsList.Add(name);
+        }
+
+    }
+    else if (commandParts.Length == 4)
+    {
+        if (guestsList.Contains(name))
+        {
+            guestsList.Remove(name);
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not in the list!");
+        }
+    }
+}
+
+foreach (string guestName in guestsList)
+{
+    Console.WriteLine(guestName);
+}
